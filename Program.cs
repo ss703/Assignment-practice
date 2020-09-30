e627cd0893fff40d711077cfcfe314a8a5e15b8d
ss703 <72144676+ss703@users.noreply.github.com>
2020-09-30 07:31:57 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n,sum=0 , r;
+            Console.WriteLine("enter a number");
+            n = Convert.ToInt32(Console.ReadLine());
+            while(n!=0)
+            {
+                r = n % 10;
+               
+                sum = sum + r;
+                n = n / 10;
+
+            }
+            Console.WriteLine("sum is="+sum);
+            Console.ReadKey();
+        }
+    }
+}
