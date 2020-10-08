1f5ca5d8cd70e0cc1be72b79b4792d34dda1d2a9
ss703 <72144676+ss703@users.noreply.github.com>
2020-10-07 21:31:45 -0700

    Add files via upload

---
+++ array/comparearray.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace compare_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num1 = { 10, 20 };
+            int[] num2 = num1;
+            if(num1==num2)
+            {
+                Console.WriteLine("Equal");
+               
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ array/jaggedarray.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2d_array
+{
+    class jaggedarray
+    {
+        static void Main(string[] args)
+        {
+            int[,] num = {
+                 { 9,8,7},//oth row
+                  { 6, 9, 5 },//1st row
+                  { 1, 2, 3}//2nd row
+            };
+            Console.WriteLine(num.GetLength(0));//num of rows
+            Console.WriteLine(num.GetLength(1));//number of col for 1st row
+            for (int i = 0; i < num.GetLength(0); i++)
+            {
+                for (int k = 0; k < num.GetLength(1); k++)
+                {
+                    Console.Write(num[i,k] + "");
+
+                }
+                Console.WriteLine();
+
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
