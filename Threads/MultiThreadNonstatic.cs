ba23d315498b8af7669a683e9ff9cbebf4326d5a
ss703 <72144676+ss703@users.noreply.github.com>
2020-10-09 02:41:05 -0700

    Add files via upload

---
+++ Threads/MultiThreadNonstatic.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Multithreadnonstatic
+{
+	class mythreads
+	{
+        
+            public int Thread1(int num1, int num2)
+            {
+
+                return num1 + num2;
+            }
+
+            public int Thred2(string str)
+            {
+                Thread.Sleep(2000);
+                return str.Length;
+            }
+
+            public string Thread3(string s1, string s2)
+            {
+                return s1 + s2;
+            }
+           
+            
+
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                mythreads obj = new mythreads();
+                int sum = 0;
+                int length = 0;
+                string s = null;
+               
+                Thread T1 = new Thread(() => { sum = obj.Thread1(2,3); });
+                Thread T2 = new Thread(() => { length = obj.Thred2("soumya"); });
+                Thread T3 = new Thread(() => { s = obj.Thread3("sou","mya"); });
+                T1.Start();
+                T2.Start();
+                T3.Start();
+              
+                //T1.Join();
+                //T2.Join();
+                //T3.Join();
+                //T4.Join();
+                Console.WriteLine("the sum of two numbers is {0}", sum);
+                Console.WriteLine("the length of the string is {0}", length);
+                Console.WriteLine("the cancatenated string is {0} ", s);
+           
+                Console.ReadKey();
+
+
+               
+
+
+		}
+		}
+	}
+
+
+        
---
+++ Threads/singlethraed.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+
+namespace singlethreadninstatic
+{
+    class singlethraed
+    {
+        static string Thread1()
+        {
+
+            return "thraed1 started";
+            
+
+
+        }
+        static string Thread2()
+        {
+            return "thraed2 started";
+        }
+        static string Thread3()
+        {
+            return "thraed3 started";
+        }
+        static void Main(string[] args)
+        {
+            string res = Thread1();
+            Console.WriteLine(res);
+            Console.WriteLine("end of thread1");
+
+            string res1 = Thread2();
+            Console.WriteLine(res1);
+            Console.WriteLine("end of thread2");
+
+            string res2 = Thread3();
+            Console.WriteLine(res2);
+            Console.WriteLine("end of thread3");
+            Console.ReadKey();
+
+        }
+    }
+}
