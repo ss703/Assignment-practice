e95dd7b19d9b7629da8d9cc1f87f06a092ba6db5
ss703 <72144676+ss703@users.noreply.github.com>
2020-10-09 04:10:56 -0700

    Add files via upload

---
+++ Threads/multithread.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Multithread
+{
+    class Program
+    {
+       
+        
+			static void Thread1()
+			{
+				for (int i = 1; i < 15; i++)
+				{
+					Console.WriteLine("Thread1 is started at the pos:" + i);
+
+				}
+			}
+			static void Thread2()
+			{
+				for (int i = 1; i < 15; i++)
+				{
+					if (i == 4)
+					{
+						Console.WriteLine("thread2 sleeping for 10 sec");
+						Thread.Sleep(10000);
+					}
+					Console.WriteLine("Thread2 is started at the pos:" + i);
+				}
+			}
+			static void Thread3()
+			{
+				for (int i = 1; i < 15; i++)
+				{
+					Console.WriteLine("Thread3 is started at the pos:" + i);
+				}
+			}
+
+			static void Main(string[] args)
+			{
+			//Main thread
+			 //create the child  thread wrt Methods involved
+				Thread T1 = new Thread(Thread1);
+				Thread T2 = new Thread(Thread2);
+				Thread T3 = new Thread(Thread3);//One main thread and 3 child Threads
+
+				T1.Start(); T2.Start(); T3.Start();
+			}
+		}
+    }
+
---
+++ Threads/singlethraedNonstatic.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+
+namespace singlethreadninstatic
+{
+    class singlethraed
+    {
+         string Thread1()
+        {
+            for(int i=1;i<=5;i++)
+            {
+                Console.WriteLine("Thread1 start at pos", +i);
+            }
+
+            return "thraed1 started";
+            
+
+
+        }
+         string Thread2()
+        {
+
+            for(int i=1;i<=5;i++)
+            {
+                if(i==4)
+                {
+                    Console.WriteLine("Thread is sleeping for few sec");
+                    Thread.Sleep(10000);
+                    
+                }
+            }
+            return "thraed2 started";
+        }
+        string Thread3()
+        {
+            for(int i=1;i<=5;i++)
+            {
+                Console.WriteLine("Thread3 start at pos", +i);
+
+            }
+            return "thraed3 started";
+        }
+        static void Main(string[] args)
+        {
+            singlethraed obj= new singlethraed();
+
+          
+            Console.WriteLine(obj.Thread1());
+            Console.WriteLine(obj.Thread2());
+            Console.WriteLine(obj.Thread3());
+
+        }
+    }
+}
