dbb841b2a4013ac649d3beeac2e02ad6f5791161
ss703 <72144676+ss703@users.noreply.github.com>
2020-10-24 00:14:49 -0700

    Add files via upload

---
+++ asp_web_form/InlineCoding.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" %>
+
+<!DOCTYPE html>
+
+<script runat="server">
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        Response.Write("Button is Working Fine!!");
+    }
+</script>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="Button1" runat="server" Text="Click ME" ForeColor="Blue" OnClick="Button1_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ asp_web_form/radio_button.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class radio_button : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Red_CheckedChanged(object sender, EventArgs e)
+    {
+        Response.Write("This is Red Color");
+    }
+
+    protected void Blue_CheckedChanged(object sender, EventArgs e)
+    {
+        Response.Write("This is Blue color");
+    }
+
+    protected void Green_CheckedChanged(object sender, EventArgs e)
+    {
+        Response.Write("This is Green color");
+    }
+
+    protected void Unnamed1_TextChanged(object sender, EventArgs e)
+    {
+        Response.Write("This is Red color");
+
+    }
+
+    protected void Unnamed2_TextChanged(object sender, EventArgs e)
+    {
+        Response.Write("This is Blue color");
+    }
+
+    protected void Unnamed3_TextChanged(object sender, EventArgs e)
+    {
+        Response.Write("This is Green color");
+    }
+
+    protected void Unnamed1_Click(object sender, EventArgs e)
+    {
+        Response.Write("this is red clr");
+    }
+
+    protected void Unnamed2_Click(object sender, EventArgs e)
+    {
+        Response.Write("this is blue clr");
+    }
+
+    protected void Unnamed3_Click(object sender, EventArgs e)
+    {
+        Response.Write("this is green clr");
+    }
+}
